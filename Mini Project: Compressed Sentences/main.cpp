b6cf1ec5993d1c8076bc19a47e9a0ad727592c1f
Cristian <cramz51@gmail.com>
2022-03-07 12:08:41 -0800

    Initial Commit

---
+++ Mini Project: Compressed Sentences/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Mini Project: Compressed Sentences
+//
+//  Created by Cristian Ramirez on 3/7/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
